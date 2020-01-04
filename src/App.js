d9894945b397e154df2399ebc945bc4c92f16449
prabhnoor <prabhnoorsingh15@gmail.com>
2020-01-04 12:42:28 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   state = {
 
     counters:[
-      {id : 1, value: 0},
+      {id : 1, value: 5},
       {id : 2, value: 0},
       {id : 3, value:0},
       {id : 4, value:0}
